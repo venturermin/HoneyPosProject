03e930b6fb30c7533f0518cbb8dbb897e1265977
JAEIN1116 <leejaein1116@gmail.com>
2017-12-07 10:15:03 +1000

    Update Button Intent

--- app/src/main/java/com/bumslap/bum/menuedit/CostSettingActivity.java
+++ app/src/main/java/com/bumslap/bum/menuedit/CostSettingActivity.java
@@ -1,21 +1,19 @@
 package com.bumslap.bum.menuedit;
 
-import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.bumslap.bum.R;
 
-import java.util.ArrayList;
-
 public class CostSettingActivity extends AppCompatActivity {
     ListView listView_cost;
+    Button MenuSetBtn, CostSetBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +31,29 @@ public class CostSettingActivity extends AppCompatActivity {
         spinnerMenu.setAdapter(adapter);
         spinnerMenu.setSelection(0);
 
+        MenuSetBtn = (Button)findViewById(R.id.MenuSetBtn);
+        CostSetBtn = (Button)findViewById(R.id.CostSetBtn);
+        MenuSetBtn.setOnClickListener(MenuCostClick);
+        CostSetBtn.setOnClickListener(MenuCostClick);
+
     }
+
+    Button.OnClickListener MenuCostClick = new View.OnClickListener() {
+        Intent mvSettingIntent;
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.MenuSetBtn:
+                    mvSettingIntent = new Intent(getApplicationContext(), MenuSettingActivity.class);
+                    startActivity(mvSettingIntent);
+                    break;
+                case R.id.CostSetBtn:
+                    mvSettingIntent = new Intent(getApplicationContext(), CostSettingActivity.class);
+                    startActivity(mvSettingIntent);
+                    break;
+            }
+
+        }
+    };
 }
 
