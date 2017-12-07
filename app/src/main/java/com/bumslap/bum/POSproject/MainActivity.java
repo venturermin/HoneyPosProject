617f5269504ca8770088e47e7c14091719aedfdb
bumslap <bumsgoh@gmail.com>
2017-12-07 14:34:17 +0900

    drawable,main UI, Login UI added

--- app/src/main/java/com/bumslap/bum/POSproject/MainActivity.java
+++ app/src/main/java/com/bumslap/bum/POSproject/MainActivity.java
@@ -19,40 +19,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BtnStart = (ImageButton)findViewById(R.id.button_Start);
+
+       BtnStart = (ImageButton)findViewById(R.id.button_Start);
         BtnStart.setOnClickListener(BtnClick);
+
+
 
         BtnPrepare = (ImageButton)findViewById(R.id.button_Prepare);
         BtnPrepare.setOnClickListener(BtnClick);
+
 
         BtnAnalysis = (ImageButton)findViewById(R.id.button_Analysis);
         BtnAnalysis.setOnClickListener(BtnClick);
 
         BtnSetting = (ImageButton)findViewById(R.id.button_Setting);
         BtnSetting.setOnClickListener(BtnClick);
+
     }
 
-   ImageButton.OnClickListener BtnClick = new View.OnClickListener() {
+
+
+    ImageButton.OnClickListener BtnClick = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             switch (view.getId()){
                 case R.id.button_Start:
-                    BtnStart.setBackgroundResource(R.drawable.button_color);
+
                     Intent intent = new Intent(getApplicationContext(), com.bumslap.bum.order.OrderActivity.class);
                     startActivity(intent);
                     break;
                 case R.id.button_Prepare:
-                    BtnStart.setBackgroundResource(R.drawable.button_color);
+
                     intent = new Intent(getApplicationContext(), com.bumslap.bum.menuedit.MenuSettingActivity.class);
                     startActivity(intent);
                     break;
                 case R.id.button_Analysis:
-                    BtnStart.setBackgroundResource(R.drawable.button_color);
+
                     intent = new Intent(getApplicationContext(), com.bumslap.bum.statistics.PieChartDataActivity.class);
                     startActivity(intent);
                     break;
                 case R.id.button_Setting:
-                    BtnStart.setBackgroundResource(R.drawable.button_color);
+
                     intent = new Intent(getApplicationContext(), com.bumslap.bum.settings.UserSettingActivity.class);
                     startActivity(intent);
                     break;
