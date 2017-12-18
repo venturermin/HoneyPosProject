8ee14c44308d18613b468d341446e3af4416b716
KwonOyoun <koy9303@gmail.com>
2017-12-18 10:41:56 +0900

    Delete Our friend.., View Pager...

--- app/src/main/java/com/bumslap/bum/order/OrderActivity.java
+++ app/src/main/java/com/bumslap/bum/order/OrderActivity.java
@@ -74,8 +74,7 @@ public class OrderActivity extends AppCompatActivity
     ArrayList<RealtimeOrder> Billordermenu;
 
 
-    ViewPager pager;
-    PagerAdapter adapter;
+
     String str_device;
     public static DBHelper dbforAnalysis;
 
@@ -91,6 +90,8 @@ public class OrderActivity extends AppCompatActivity
     OrderListAdapter orderListAdapter;
 
     DBforAnalysis newdbforAnalysis;
+
+
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -100,7 +101,6 @@ public class OrderActivity extends AppCompatActivity
         setContentView(R.layout.activity_order);
         // setContentView()가 호출되기 전에 setRequestedOrientation()이 호출되어야 함
         //setTitle("오늘도 달려 보세");
-        init();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         gridView = (GridView) findViewById(R.id.gridview);
@@ -136,52 +136,13 @@ public class OrderActivity extends AppCompatActivity
         //billRecyclerView = (RecyclerView) findViewById(R.id.list_order);
         //billRecyclerView.setLayoutManager(layoutManager);
 
-        //pager = (ViewPager) findViewById(R.id.order_pager);
 
 
 
-/*
-        pager.setOnTouchListener(new ViewPager.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                pager.SCROLL_INDICATOR_RIGHT.
-                return false;
-            }
-        });
-*/
-        ClickableViewPager viewPager = (ClickableViewPager)findViewById(R.id.order_pager);
-        pager.setAdapter(adapter);
-        viewPager.setOnItemClickListener(new ClickableViewPager.OnItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-                int i = position;
-            }
-        });
-      /*
-      pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-            int i = position;
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-*/
         OrderList = new ArrayList<HashMap<String, Integer>>();
         Ordermap = null;
         Ordermap = new HashMap<String, Integer>();
 
-        //pager.SimpleOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
-
-        //});
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id){
@@ -225,204 +186,6 @@ public class OrderActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-
-    public int differentDensityAndScreenSize(Context context) {
-        int value = 20;
-
-        if ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_SMALL) {
-            switch (context.getResources().getDisplayMetrics().densityDpi) {
-                case DisplayMetrics.DENSITY_LOW:
-
-                    value = 20;
-                    break;
-                case DisplayMetrics.DENSITY_MEDIUM:
-
-                    value = 20;
-                    break;
-                case DisplayMetrics.DENSITY_HIGH:
-
-                    value = 20;
-                    break;
-                case DisplayMetrics.DENSITY_XHIGH:
-
-                    value = 20;
-                    break;
-                case DisplayMetrics.DENSITY_XXHIGH:
-
-                    value = 20;
-                    break;
-                case DisplayMetrics.DENSITY_XXXHIGH:
-
-                    value = 20;
-                    break;
-                case DisplayMetrics.DENSITY_TV:
-
-                    value = 20;
-                    break;
-                default:
-
-                    value = 20;
-                    break;
-            }
-
-        } else if ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_NORMAL) {
-            switch (context.getResources().getDisplayMetrics().densityDpi) {
-                case DisplayMetrics.DENSITY_LOW:
-
-                    str_device = "normal-ldpi";
-                    value = 82;
-                    break;
-                case DisplayMetrics.DENSITY_MEDIUM:
-
-                    value = 82;
-                    str_device = "normal-mdpi";
-                    break;
-                case DisplayMetrics.DENSITY_HIGH:
-
-                    str_device = "normal-hdpi";
-                    value = 82;
-                    break;
-                case DisplayMetrics.DENSITY_XHIGH:
-
-                    str_device = "normal-xhdpi";
-                    value = 90;
-                    break;
-                case DisplayMetrics.DENSITY_XXHIGH:
-
-                    str_device = "normal-xxhdpi";
-                    value = 96;
-                    break;
-                case DisplayMetrics.DENSITY_XXXHIGH:
-
-                    str_device = "normal-xxxhdpi";
-                    value = 96;
-                    break;
-                case DisplayMetrics.DENSITY_TV:
-
-                    str_device = "normal-tvmdpi";
-                    value = 96;
-                    break;
-                default:
-
-                    str_device = "normal-unknown";
-                    value = 82;
-                    break;
-            }
-        } else if ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE) {
-            switch (context.getResources().getDisplayMetrics().densityDpi) {
-                case DisplayMetrics.DENSITY_LOW:
-
-                    value = 78;
-                    break;
-                case DisplayMetrics.DENSITY_MEDIUM:
-
-                    value = 78;
-                    break;
-                case DisplayMetrics.DENSITY_HIGH:
-
-                    value = 78;
-                    break;
-                case DisplayMetrics.DENSITY_XHIGH:
-
-                    value = 125;
-                    break;
-                case DisplayMetrics.DENSITY_XXHIGH:
-
-                    value = 125;
-                    break;
-                case DisplayMetrics.DENSITY_XXXHIGH:
-
-                    value = 125;
-                    break;
-                case DisplayMetrics.DENSITY_TV:
-
-                    value = 125;
-                    break;
-                default:
-
-                    value = 78;
-                    break;
-            }
-
-        } else if ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
-            switch (context.getResources().getDisplayMetrics().densityDpi) {
-                case DisplayMetrics.DENSITY_LOW:
-
-                    value = 125;
-                    break;
-                case DisplayMetrics.DENSITY_MEDIUM:
-
-                    value = 125;
-                    break;
-                case DisplayMetrics.DENSITY_HIGH:
-
-                    value = 125;
-                    break;
-                case DisplayMetrics.DENSITY_XHIGH:
-
-                    value = 125;
-                    break;
-                case DisplayMetrics.DENSITY_XXHIGH:
-
-                    value = 125;
-                    break;
-                case DisplayMetrics.DENSITY_XXXHIGH:
-
-                    value = 125;
-                    break;
-                case DisplayMetrics.DENSITY_TV:
-
-                    value = 125;
-                    break;
-                default:
-
-                    value = 125;
-                    break;
-            }
-        }
-
-        return value;
-    }
-    private List<Fragment> getFragments() {
-
-        List<Fragment> fList = new ArrayList<Fragment>();
-
-        for (int i = 0  ; i < 10; i++){
-            fList.add(PageFragment.create(i));
-        }
-
-        return fList;
-
-    }
-
-    private void init() {
-        pager = (ViewPager) findViewById(R.id.order_pager);
-        differentDensityAndScreenSize(getApplicationContext());
-        List<Fragment> fragments = getFragments();
-        pager.setAdapter(adapter);
-        pager.setClipToPadding(false);
-
-
-        if (str_device.equals("normal-hdpi")){
-            pager.setPadding(0, 0, 0, 0);
-        }else if (str_device.equals("normal-mdpi")){
-            pager.setPadding(0, 0, 0, 0);
-        }else if (str_device.equals("normal-xhdpi")){
-            pager.setPadding(0, 0, 0, 0);
-        }else if (str_device.equals("normal-xxhdpi")){
-            pager.setPadding(0, 0, 0, 0);
-        }else if (str_device.equals("normal-xxxhdpi")){
-            pager.setPadding(0, 0, 0, 0);
-        }else if (str_device.equals("normal-unknown")){
-            pager.setPadding(0, 0, 0, 0);
-        }else {
-
-        }
-
-        adapter = new PagerAdapter(getSupportFragmentManager(), fragments);
-        pager.setPageTransformer(true, new ViewpagerTransformer());
-        pager.setAdapter(adapter);
-    }
     @Override
     public void onPause(){
         super.onPause();
